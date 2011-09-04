88139d5884e75d76a6de9188242b75a5e1254bd2
Phillip Cunningham <matt1988@live.com>
2011-09-03 23:24:26 -0500

    for v0.5 added permission nodes

--- src/me/Matt1988/lwckeys/LWCKeysModuleListener.java
+++ src/me/Matt1988/lwckeys/LWCKeysModuleListener.java
@@ -21,7 +21,9 @@ public class LWCKeysModuleListener extends JavaModule {
 		
 		Protection protection = event.getProtection();
 		Player player = event.getPlayer();
-		
+		if (plugin.isPlayerDisabled(protection.getOwner())) {
+			return;
+		}
 		ItemStack itemInHand = player.getItemInHand();
 		
 			//Block's Protection ID
@@ -32,21 +34,23 @@ public class LWCKeysModuleListener extends JavaModule {
 			 * 
 			 *  A. The player has no items in hand.
 			 *  B. The player interacts with a protected block while sneaking 
-			 *  C. The player owns the protected block OR is an op.*/
-			 				
-			if(player.isSneaking())
+			 *  C. The player owns the protected block OR is an op.
+			 *  D. The player has permission to make keys*/
+			if(player.isSneaking())//make sure player is sneaking
 			{
-				if (itemInHand.getTypeId() == 0)
+				if (itemInHand.getTypeId() == 0)//make sure hand is empty
 				{
-					if (protection.getOwner().equalsIgnoreCase(player.getName()) || player.isOp()) {
-						if(player.isOp() 
+					if (protection.getOwner().equalsIgnoreCase(player.getName()) || player.isOp()) {// check if player is owner of protection or is an op
+						if(player.isOp()
 								&& !(protection.getOwner().equalsIgnoreCase(player.getName())) 
-								&& !LWCKeys.OP_MAKE_KEY)
+								&& !LWCKeys.OP_MAKE_KEY) //return if ops don't have the ability to make keys from others chests.
 							return;
-						ItemStack keyItem = new ItemStack(LWCKeys.KEY_ITEM, 1, (short) mod(protectionID), (byte) mod(protectionID));
-						player.getInventory().setItemInHand(keyItem);
-						player.sendMessage(plugin.textParse(LWCKeys.KEY_CREATED_TEXT, protection));
-						event.setResult(Result.CANCEL);
+						if (plugin.playerHasPermission(player, "lwckeys.general.create", false)) {
+							ItemStack keyItem = new ItemStack(LWCKeys.KEY_ITEM, 1, (short) mod(protectionID), (byte) mod(protectionID));
+							player.getInventory().setItemInHand(keyItem); //create the key to the corresponding protection block in the empty hand slot.
+							player.sendMessage(plugin.textParse(LWCKeys.KEY_CREATED_TEXT, protection)); //Parse and send the KEY_CREATED_TEXT.
+							event.setResult(Result.CANCEL); //CANCEL the interaction with the object.
+						}
 						
 						return;
 					}
@@ -62,22 +66,22 @@ public class LWCKeysModuleListener extends JavaModule {
 			 * 
 			 * Note: Minecraft's limitations prevent there from being more than 256 unique keys so
 			 * I use a quick and dirty hack to get around it (mod the protection ID by 256 before I compare it with the Key Durability..)
-			 * The huge drawback to this was duplicate keys.
-			 * Hoping to find a more elegant solution soon. */
-			 
+			 * The huge drawback to this is duplicate keys.
+			 * Hoping to find a more elegant solution soon. */	
+			if (plugin.playerHasPermission(player, "lwckeys.general.unlock", false)) {
+				if (itemInHand.getTypeId() != LWCKeys.KEY_ITEM) // returns if user does not have KEY_ITEM in hand.
+					return;
+				int itemKeyId = itemInHand.getDurability(); //get the ID (Durability) of the key in hand
 			
-			if (itemInHand.getTypeId() != LWCKeys.KEY_ITEM)
-				return;
-			int itemKeyId = itemInHand.getDurability();
 			
+			//If the Key ID(Durability) is below 0, make it useable.
+				if (itemKeyId < 0) 
+					itemKeyId = 256 + itemKeyId;			
 			
-			//If the Key ID(Durability) is a - number, make it useable.
-			if (itemKeyId < 0) 
-				itemKeyId = 256 + itemKeyId;			
-			
-			if (mod(protectionID) == itemKeyId && !(event.canAccess())) {
-				event.setResult(Result.ALLOW);
-				player.sendMessage(plugin.textParse(LWCKeys.KEY_UNLOCK_TEXT, protection));
+				if (mod(protectionID) == itemKeyId && !(event.canAccess())) {
+					event.setResult(Result.ALLOW);
+					player.sendMessage(plugin.textParse(LWCKeys.KEY_UNLOCK_TEXT, protection));
+				}
 			}
 			
 		
@@ -86,13 +90,14 @@ public class LWCKeysModuleListener extends JavaModule {
 	}
 	
 	
-	
-	/*
-	 * Simple Modulo operation, takes the Protection ID of the protected block
+	/**
+	 *  Simple Modulo operation, takes the Protection ID of the protected block
 	 * and subtracts it by 256 until the  ID is less than 256
 	 * The remainder afterwards represents the id of the key that can be used to open that protection.
+	 * 
+	 * @param i
+	 * @return
 	 */
-	
 	public int mod(int i) {
 		int inte = i;
 		
